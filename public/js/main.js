1eb628b76b6acd73ed6a1434db45e09eaf4f33fc
webnetkz <dbqqbq@gmail.com>
2021-12-16 10:53:06 +0600

    new life for pwareduced

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,4 @@
 "use strict";
 import {parallaxScroll} from './moduls/parallaxScroll.js';
 parallaxScroll('.slider');
-import {darkMod} from './moduls/darkMod.js';
-//darkMod();
 
-function showMenu(elem) {
-    let menu = document.querySelector('menu');
-    let closeMenuMob = document.querySelector('.closeMenuMob');
-
-    elem.style.opacity = '0';
-    closeMenuMob.style.right = '10px';
-    menu.style.left = '0px';
-}
-function closeMenu(elem) {
-    let menu = document.querySelector('menu');
-    let menuMob = document.querySelector('.menuMob');
-
-    menuMob.style.opacity = '1';
-    elem.style.right = '-20%';
-    menu.style.left = '-100%';
-}
--- public/js/moduls/darkMod.js
+++
@@ -1,5 +0,0 @@
-export function darkMod() {
-    let darkStyles = document.createElement('style');
-    darkStyles.innerText = ':root {filter: invert(100%)}';
-    document.body.appendChild(darkStyles)
-}
